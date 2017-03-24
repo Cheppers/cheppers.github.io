14df5195a649ab6eb555a4dfeed8406bbf72ee30
Balázs Baumgartner <balazs.baumgartner@cheppers.com>
2017-03-24 18:09:27 +0100

    Added axios for HTTP requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,48 +1,17 @@
-import { checkHttpStatus, parseJSON } from '../utils/utils';
+import axios from 'axios';
 
 export function getRepos() {
-  return fetch('https://pumpa.cheppersdev.com/repos', {
-    method: 'get',
-    headers: {
-      Accept: 'application/vnd.github.v3+json',
-    },
-  })
-  .then(checkHttpStatus)
-  .then(parseJSON)
-  .then((resp) => {
-    const repos = [];
-    try {
-      resp.forEach((repo) => {
-        repos.push({
-          id: repo.id,
-          name: repo.name,
-          description: repo.description,
-          language: repo.language,
-          html_url: repo.html_url,
-        });
-      });
-    } catch (err) {
-      console.log(err); // eslint-disable-line
-    }
-
-    return repos;
-  })
+  return axios.get('https://pumpa.cheppersdev.com/repos')
+  .then(response => (response.data))
   .catch((error) => {
-    console.log(error); // eslint-disable-line
+    console.err(error); // eslint-disable-line
   });
 }
 
 export function getMembers() {
-  return fetch('https://pumpa.cheppersdev.com/members', {
-    method: 'get',
-    headers: {
-      Accept: 'application/vnd.github.v3+json',
-    },
-  })
-  .then(checkHttpStatus)
-  .then(parseJSON)
-  .then(response => (response))
+  return axios.get('https://pumpa.cheppersdev.com/members')
+  .then(response => (response.data))
   .catch((error) => {
-    console.log(error); // eslint-disable-line
+    console.err(error); // eslint-disable-line
   });
 }
